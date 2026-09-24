e31a8377e1e39de32d501a2053f505c577dda445
that-kimani <that.kimani@gmail.com>
2026-09-24 18:29:12 +0300

    Reorder portfolio items for improved display consistency

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,21 +7,21 @@ export default function Portfolio() {
   const portfolioItems = [
     {
       id: 1,
+      title: 'I built a Retail High Frequency Trading Kit',
+      category: 'Finance / Software Development',
+      image: candle_chart,
+      url: 'https://github.com/that-kimani/retail-hft-kit'
+    },
+    {
+      id: 2,
       title: 'Using Sales Data to Predict Demand with Machine Learning',
       category: 'Financial analysis / ML',
       image: transactions_to_forecasts,
       url: 'https://medium.com/@that.kimani/transactions-to-forecasts-sales-analysis-and-forecasting-using-ml-6b3e4f797693'
     },
     {
-      id: 2,
-      title: 'A Retail High Frequency Trading Kit',
-      category: 'Finance / Software Development',
-      image: candle_chart,
-      url: 'https://github.com/that-kimani/retail-hft-kit'
-    },
-    {
       id: 3,
-      title: 'Using AI in HR processes',
+      title: 'Can AI streamline HR processes?',
       category: 'UI/UX Design',
       image: ai_agents_and_hr,
       url: 'https://github.com/that-kimani/pegasus-recruitment-website'
