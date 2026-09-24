2edd967d999ac7c2e42605410a1de98450bea00b
that-kimani <that.kimani@gmail.com>
2026-09-24 18:23:40 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,30 +1,31 @@
 import './Portfolio.css'
 import transactions_to_forecasts from '../../assets/project_covers/transactions_to_forecasts.png'
 import ai_agents_and_hr from '../../assets/project_covers/ai_agents_and_hr.jpg'
+import candle_chart from '../../assets/project_covers/candle_chart.jpg'
 
 export default function Portfolio() {
   const portfolioItems = [
     {
       id: 1,
-      title: 'Sales Data to Demand Forecasts with Machine Learning',
+      title: 'Using Sales Data to Predict Demand with Machine Learning',
       category: 'Financial analysis / ML',
       image: transactions_to_forecasts,
       url: 'https://medium.com/@that.kimani/transactions-to-forecasts-sales-analysis-and-forecasting-using-ml-6b3e4f797693'
     },
     {
       id: 2,
+      title: 'A Retail High Frequency Trading Kit',
+      category: 'Finance / Software Development',
+      image: candle_chart,
+      url: 'https://github.com/that-kimani/retail-hft-kit'
+    },
+    {
+      id: 3,
       title: 'Using AI in HR processes',
       category: 'UI/UX Design',
       image: ai_agents_and_hr,
       url: 'https://github.com/that-kimani/pegasus-recruitment-website'
     },
-    // {
-    //   id: 3,
-    //   title: 'Brand Identity',
-    //   category: 'Branding',
-    //   image: '🎨',
-    //   url: 'https://medium.com/@yourprofile/branding'
-    // },
     // {
     //   id: 4,
     //   title: 'Dashboard Analytics',
